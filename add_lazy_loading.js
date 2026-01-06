79a6852a0538bf22e194e6b78aaa44d9fa71d3f5
sukhpreet001007 <sukhpreet001007@gmail.com>
2026-01-06 17:24:48 +0530

    inner page, surface responsive, cards

--- add_lazy_loading.js
+++ add_lazy_loading.js
@@ -4,7 +4,6 @@ const path = require('path');
 const filePath = path.join(process.cwd(), 'index.html');
 let content = fs.readFileSync(filePath, 'utf8');
 
-// We want to skip the first ~350 lines (Hero section)
 const heroCutoff = 350;
 
 const newline = content.includes('\r\n') ? '\r\n' : '\n';
